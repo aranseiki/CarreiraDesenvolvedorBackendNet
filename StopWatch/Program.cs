c6be1e492054a36bac0ca4a6f21d96c649e50a10
Allan de Oliveira Almeida <techall@hotmail.com.br>
2023-01-08 10:55:51 -0300

    Implementado base do menu

--- StopWatch/Program.cs
+++ StopWatch/Program.cs
@@ -1,6 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
-static void Start() {
-    int time = 10;
+static void Memu() {
+    Console.Clear();
+    Console.WriteLine("-----------------------------------");
+    Console.WriteLine(" ");
+    Console.WriteLine("S = Segundo");
+    Console.WriteLine("M = Minuto");
+    Console.WriteLine("0 = Sair");
+    Console.WriteLine("Quanto tempo deseja contar?");
+    Console.WriteLine(" "); 
+    Console.WriteLine("-----------------------------------");
+}
+
+static void Start(int time) {
     int currentTime = 0;
 	while (currentTime != time) {
         Console.Clear();
@@ -8,6 +19,10 @@ static void Start() {
         Console.WriteLine(currentTime);
         Thread.Sleep(1000);
     }
+
+    Console.Clear();
+    Console.WriteLine("Stopwatch finalizado.");
+    Thread.Sleep(2500);
 }
 
-Start();
+Memu();
