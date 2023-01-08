398e99fd93200933111a386d1f59f184ac3f6be1
Allan de Oliveira Almeida <techall@hotmail.com.br>
2023-01-08 15:38:47 -0300

    Criado base da função Edit.

--- TextEditor/Program.cs
+++ TextEditor/Program.cs
@@ -1,2 +1,51 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+static void Menu() {
+    Console.Clear();
+    Console.WriteLine("-----------------------------------");
+    Console.WriteLine(" ");
+    Console.WriteLine("O que você deseja fazer?");
+    Console.WriteLine("0 - Sair");
+    Console.WriteLine("1 - Abrir arquivo");
+    Console.WriteLine("2 - Criar um novo arquivo");
+    Console.WriteLine(" ");
+    Console.WriteLine("-----------------------------------");
+
+    short? option = short.Parse(Console.ReadLine());
+
+    switch (option) {
+        case 0:
+            Environment.Exit(0);
+            break;
+        case 1:
+            open();
+            break;
+        case 2:
+            Edit();
+            break;
+        default:
+            Menu();
+            break;
+    }
+
+}
+
+static void open() {
+
+}
+
+static void Edit() {
+    Console.Clear();
+    Console.WriteLine("Pressione ESC para sair.");
+    Console.WriteLine("Digite seu texto: ");
+    Console.WriteLine("-----------------------------------");
+    Console.WriteLine(" ");
+
+    string text = "";
+
+    while (Console.ReadKey().Key != ConsoleKey.Escape) {
+
+    }
+
+}
+
+Menu();
