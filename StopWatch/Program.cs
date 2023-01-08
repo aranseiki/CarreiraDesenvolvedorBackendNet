b07fd9246303c598034ec5d2ea254fc2e03e8fc3
Allan de Oliveira Almeida <techall@hotmail.com.br>
2023-01-08 11:43:30 -0300

    Adicionado mensagem de preparação antes da contagem.

--- StopWatch/Program.cs
+++ StopWatch/Program.cs
@@ -11,6 +11,7 @@ static void Menu() {
     Console.WriteLine("Ex.: ");    
     Console.WriteLine("     10s = 10 segundos");
     Console.WriteLine("     1m = 1 minuto");
+    Console.WriteLine(" ");
     Console.WriteLine("Quanto tempo deseja contar?");
     Console.WriteLine(" ");
     Console.WriteLine("-----------------------------------");
@@ -19,6 +20,7 @@ static void Menu() {
     if (data == "" || data == "0") {
         Environment.Exit(0);
     }
+
     char type = char.Parse(data.Substring(data.Length - 1, 1));
     int time = int.Parse(data.Substring(0, data.Length - 1));
     int multiplier = 1;
@@ -31,7 +33,20 @@ static void Menu() {
         multiplier = 60;
     }
 
-    Start(time * multiplier);
+    PreStart(time * multiplier);
+}
+
+static void PreStart(int time) {
+    Console.Clear();
+    Console.WriteLine("Ready...");
+    Thread.Sleep(1000);
+    Console.WriteLine("Set...");
+    Thread.Sleep(1000);
+    Console.WriteLine("Go...");
+    Thread.Sleep(2500);
+    Console.Clear();
+
+    Start(time);
 }
 
 static void Start(int time) {
