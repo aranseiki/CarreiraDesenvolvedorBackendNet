615a97657a6538aead8ed5e7fe057f23fba37062
Allan de Oliveira Almeida <techall@hotmail.com.br>
2023-01-15 11:22:40 -0300

    Implementado caixa de menu

--- EditorHTML/Program.cs
+++ EditorHTML/Program.cs
@@ -5,6 +5,7 @@ namespace EditorHTML {
     class Program {
         static void Main(string[] args) {
             Menu.Show();
+            Console.ReadKey();
         }
     }
 }
--- EditorHTML/menu.cs
+++ EditorHTML/menu.cs
@@ -6,6 +6,36 @@ namespace EditorHTML {
             Console.Clear();
             Console.BackgroundColor = ConsoleColor.Blue;
             Console.ForegroundColor = ConsoleColor.Black;
+            DrawScreen();
+        }
+
+        public static void DrawScreen() {
+            separateMenu();
+
+            int columnsPipes = 10;
+            for (int lines = 0; lines < columnsPipes; lines++) {
+                Console.Write("|");
+
+                int columnsSpaces = 30;
+                for (int spaces = 0; spaces < columnsSpaces; spaces++) {
+                    Console.Write(" ");
+                }
+                Console.Write("|");
+                Console.Write('\n');
+            }
+
+            separateMenu();
+        }
+
+        public static void separateMenu() {
+            Console.Write("+");
+            int columnsHiphens = 30;
+            for (int item = 0; item < columnsHiphens; item++) {
+                Console.Write("-");
+            }
+            Console.Write("+");
+
+            Console.Write('\n');
         }
     }
 }
