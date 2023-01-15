3d4d0872479642c67f97015ea20feefefef5ac4f
Allan de Oliveira Almeida <techall@hotmail.com.br>
2023-01-15 10:54:15 -0300

    Início do projeto EditorHTML

---
+++ EditorHTML/menu.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EditorHTML {
+    public static class Menu {
+        public static void Show() {
+            Console.Clear();
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.ForegroundColor = ConsoleColor.Black;
+        }
+    }
+}
