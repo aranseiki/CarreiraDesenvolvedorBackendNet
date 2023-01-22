10ae0fd27ef7399777bcc9c32ccc5e1ff5f370b3
Allan de Oliveira Almeida <techall@hotmail.com.br>
2023-01-22 16:23:28 -0300

    Implementado usabilidade no Program.cs

--- Fundamentos da Orientacao a Objetos/Balta/Program.cs
+++ Fundamentos da Orientacao a Objetos/Balta/Program.cs
@@ -12,5 +12,31 @@ internal class Program {
             Console.WriteLine(article.Title);
             Console.WriteLine(article.Url);
         }
+
+        var courses = new List<Course>();
+        var coursesOOP = new Course("Fundamentos OOP", "fundamentos-oop");
+        var coursesCSharp = new Course("Fundamentos C#", "fundamentos-csharp");
+        var coursesAspNet = new Course("Fundamentos ASP.NET", "fundamentos-aspnet");
+        courses.Add(coursesOOP);
+        courses.Add(coursesCSharp);
+        courses.Add(coursesAspNet);
+
+        var careers = new List<Career>();
+        var careerDotNet = new Career("Especialista .Net", "especialista-dotnet");
+        var careerItem = new CareerItem(1, "Comece por aqui", "", null);
+        var careerItem2 = new CareerItem(2, "Aprenda OOP", "", null);
+        var careerItem3 = new CareerItem(3, "Aprenda .Net", "", null);
+        careerDotNet.Items.Add(careerItem2);
+        careerDotNet.Items.Add(careerItem3);
+        careerDotNet.Items.Add(careerItem);
+        careers.Add(careerDotNet);
+
+        foreach (var career in careers) {
+            Console.WriteLine(career.Title);
+
+            foreach (var item in career.Items.OrderBy(x => x.Order)) {
+                Console.WriteLine($"{item.Order} - {item.Title}");
+            }
+        }
     }
-}
+}   
