587945d499f9d6f2ca7b8e72524fc481d5affce7
Allan de Oliveira Almeida <techall@hotmail.com.br>
2023-01-22 18:11:20 -0300

    Implementando base do tratamento de exceções

--- Fundamentos da Orientacao a Objetos/Balta/NotificationContext/Notifiable.cs
+++ Fundamentos da Orientacao a Objetos/Balta/NotificationContext/Notifiable.cs
@@ -4,11 +4,13 @@ namespace Balta.NotificationContext {
     public abstract class Notifiable {
         public List<Notification>? Notifications { get; set; }
 
-        public void Add(Notification notification) {
+        public void AddNotification(Notification notification) {
             Notifications?.Add(notification);
         }
-        public void AddRange(IEnumerable<Notification> notifications) {
+        public void AddNotifications(IEnumerable<Notification> notifications) {
             Notifications?.AddRange(notifications);
         }
+
+        public bool IsInvalid => Notifications.Any() ;
     }
 }
