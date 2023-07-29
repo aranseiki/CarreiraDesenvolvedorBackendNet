3335a9fc26782a99956dbcc883c94df12c8e1bb4
Allan de Oliveira Almeida <techall@hotmail.com.br>
2023-07-29 19:08:37 -0300

    Iniciando novo curso

---
+++ Acesso à dados com .NET, C#, Dapper e SQL Server/BaltaDataAccess/Program.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Data.SqlClient;
+
+
+class Program {
+    static void Main(string[] args){
+        const string connectionString
+        = "Server=localhost,1433;Database=balta;User ID=sa;Password=1q2w3e4r@#$";
+        Console.WriteLine("Hello World!");
+    }
+}
